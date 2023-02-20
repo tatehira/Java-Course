68d03de052dc0477d9692b73b416ec82902a4efe
Nick Tatehira <niickcostta00@outlook.com>
2023-02-20 00:42:14 -0400

    first comit

---
+++ POO-Java.java
@@ -0,0 +1,7 @@
+Aula 01 - O que é POO?
+	- Antes a programação era baixo nivel e somente eng programava
+	- Programação linear
+	- Programação Estruturada
+	- Programação Modular
+	- Programação Modular
+	- Criador da POO: Alan Kay
