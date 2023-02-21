5031b99e16bc392f71a2af336e317abf2557d790
Nick Tatehira <niickcostta00@outlook.com>
2023-02-21 02:26:49 -0400

    1- Whats is Object

--- POO-Java.java
+++ POO-Java.java
@@ -1,7 +1,65 @@
-Aula 01 - O que é POO?
-	- Antes a programação era baixo nivel e somente eng programava
-	- Programação linear
-	- Programação Estruturada
-	- Programação Modular
-	- Programação Modular
-	- Criador da POO: Alan Kay
+Aula 01 - What is Object
+	- Using how idea, an pen would be a Object and mold this class.
+	- Class its composed for questions
+		1- Characteristics (properthy)
+			- Color 
+			- Style
+			- Design
+		2- thing i do (Method)
+			- He write
+			- To Color
+			- Cover and Uncover
+		3- How i am now?(status - action)
+			- Writing
+			- empty(void)
+			- Characteristics now
+
+	- All Objects comes from a Class (mold)
+
+	Class Pen
+		model: Character bic
+		color: Character Blue
+		tip: float 0.5
+		copped: bool flase
+
+		Method Write()
+			if ()
+			else
+		endWrite
+		Method copped()
+			if()
+			else
+		endCopped
+
+		status
+			bic
+			blue
+			0.5
+			false
+	endClass
+
+	- Who is Instance?
+		-> When are you generate obj from a class
+		-> No transformation class - class generate obj
+
+	- How to instance an class?
+		-> pen = new Pen (Generate class an Object)
+		-> pen.color = "blue"
+		-> tip: 0.5
+		-> pen.copped = false
+		-> Method copped()
+
+		- pen2 = new Pen
+		- pen2.color = "red"
+		- tip: 1.0
+		- pen.copped: true
+		- Method Write()
+
+	Class: Define atributes, Method etc what will it be 
+			forwand for Object
+
+	Object: instance of class
+
+	Abstation: 
+		- EX: When player you hide details that won't be needed. 
+		Example: weights and heights of students
